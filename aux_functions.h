341c9dce6e986d4f7f9dbb1f604c607ae29355a8
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-04-01 19:58:56 +0000

    new header file

---
+++ aux_functions.h
@@ -0,0 +1,9 @@
+#ifndef AUX_FUNCTIONS_H
+#define AUX_FUNCTIONS_H
+
+/* Prototypes  */
+void *_calloc(char *p, int size);
+int _strlen(char *buff);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
+#endif
