12dd9019aa6a1de31947fd45f88b3e4d619952ce
tshuna332 <tmmi333gmail.com>
2021-04-01 16:19:23 +0000

    add prot

--- our_header.h
+++ our_header.h
@@ -9,6 +9,7 @@
 
 void *_calloc(void *p, int size);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void _strlen(char *buff);
 void add_aux_to_fill(char *fill, char *aux, int aux_size);
 
 #endif /* OUR_HEADER_H */
