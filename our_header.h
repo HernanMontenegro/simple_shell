1fa25cbcd1deddd2e754549810eddf8def1bf792
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-04-01 17:55:19 +0000

    header with the internal commands struct

--- our_header.h
+++ our_header.h
@@ -1,12 +1,28 @@
 #ifndef OUR_HEADER_H
 #define OUR_HEADER_H
 
+/* Libraries to use */
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/stat.h>
 
+/**
+* struct in_com - internal commands
+* @command: the command char pointer
+* @f: the pointer to a function
+* -----------------------------------------
+* Description: Manages the internal commands
+*/
+#typedef struct in_com
+{
+	char *command;
+	void (*f)(int ac, char **av);
+
+} internal_commands;
+
+/* Prototypes  */
 int _getline(char **fill, int *n, int where_read);
 void *_calloc(char *p, int size);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
