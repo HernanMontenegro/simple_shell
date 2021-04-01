bb7435e64e4c148dbb2b1553f54f59cdf06c2810
Hernan Montenegro <77987989+HernanMontenegro@users.noreply.github.com>
2021-04-01 12:53:57 -0700

    Update _getline.c

--- _getline.c
+++ _getline.c
@@ -46,27 +46,6 @@ int _getline(char **fill, int *n, int where_read)
 }
 
 /**
-* _calloc - request a space in memory and assign to 0 each one
-* @p: pointer to set
-* @size: amount of space to reserve
-* ----------------------------------------------
-* Return: void
-*/
-void *_calloc(char *p, int size)
-{
-	int i;
-
-	p = malloc(size);
-	if (!p)
-		return (NULL);
-
-	for (i = 0; i < size; i++)
-		p[i] = 0;
-
-	return (p);
-}
-
-/**
 * add_aux_to_fill - add aux to our fill in _getline
 * @fill: the fill char pointer
 * @aux: the aux char pointer
@@ -89,68 +68,4 @@ char *add_aux_to_fill(char *fill, char *aux, int aux_size)
 	fill[i] = '\0';
 
 	return (fill);
-}
-
-/**
-* _strlen - counts the length of a string
-* @buff: the buffer pointer
-* ---------------------------------
-* Return: the length of the string
-*/
-int _strlen(char *buff)
-{
-	int len;
-
-	if (!buff)
-		return (0);
-
-	for (len = 0; buff[len] != '\0'; len++)
-	{
-	}
-
-	return (len);
-}
-
-/**
-* _realloc - re size array
-* @ptr: string
-* @old_size: int
-* @new_size: uns int
-*
-* Return: void pointer
-*/
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
-{
-	unsigned int i, cal;
-	char *p;
-	char *new_ptr;
-
-	if (ptr == NULL)
-	{
-		p = malloc(new_size);
-		if (p == NULL)
-			return (NULL);
-		for (i = 0; i < new_size; i++)
-			p[i] = 0;
-		return (p);
-	}
-	else if (new_size == 0)
-	{
-		free(ptr);
-		return (NULL);
-	}
-	else if (new_size == old_size)
-		return (ptr);
-	if (new_size > old_size)
-		cal = old_size;
-	else
-		cal = new_size;
-	p = malloc(new_size);
-	if (p == NULL)
-		return (NULL);
-	new_ptr = ptr;
-	for (i = 0; i < cal; i++)
-		p[i] = new_ptr[i];
-	free(ptr);
-	return (p);
 }
