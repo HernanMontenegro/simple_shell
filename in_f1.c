e9d099594f062be490b89d3ff5ba66c084695080
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-04-04 20:48:47 +0000

    internal commands start

---
+++ in_f1.c
@@ -0,0 +1,6 @@
+#include "our_header.h"
+
+void cmd_exit(int ac, char **av)
+{
+    
+}
---
+++ internal_cmds.h
@@ -0,0 +1,10 @@
+#ifndef INTERNAL_CMDS_H
+#define INTERNAL_CMDS_H
+
+/* ==========Prototypes========== */
+/* in_f1.c */
+void cmd_exit(int ac, char **av);
+
+/* ==============================  */
+
+#endif
