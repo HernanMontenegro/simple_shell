08dffd6790a95bf629b3c8c2c72f5c224c69d72b
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-04-04 20:56:41 +0000

    _atoi created

--- aux_f1.c
+++ aux_f1.c
@@ -115,3 +115,35 @@ char *_getenv(char *env_name)
 	/* Put the var content in the str directly */
 	return (aux[1]);
 }
+
+/**
+ * _atoi - convert a string to it's integer value
+ * @s: the string given
+ * ------------------------------------
+ * Return: void
+ */
+int _atoi(char *s)
+{
+	int res = 0, sign = 1, i = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		int firstDigit_encounter = 0;
+
+		if(s[i] == ';')
+			break;
+		if(s[i] >= '0' && s[i] <= '9')
+		{
+			res = res * 10 + s[i] - '0';
+			firstDigit_encounter = 1;
+		}
+
+		if(firstDigit_encounter == 1)
+		{
+			if(s[i - 1] == '-')
+				sign = -1;
+		}
+	}
+
+	return sign * res;
+}
--- aux_functions.h
+++ aux_functions.h
@@ -13,7 +13,7 @@ void *_calloc(char *p, int size);
 int _strlen(char *buff);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 char *_getenv(char *env_name);
-/* Empty Slot */
+int _atoi(char *s);
 
 /* aux_f2.c */
 char *_strcpy(char *src);
